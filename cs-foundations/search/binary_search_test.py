98bea686537c5e216d205fc881a3d2a6c0d618f5
Doc Duncan <triplerockmentality@icloud.com>
2020-08-10 16:35:26 -0400

    DMD (D) binary search

---
+++ cs-foundations/search/binary_search_test.py
@@ -0,0 +1,36 @@
+import unittest
+import binary_search
+
+
+class SearchAlgorithmsTest(unittest.TestCase):
+    def test_binary_search_even_true(self):
+        arr = [5,9,10,73]
+        val = 73
+        min_pos = 0
+        max_pos = len(arr) - 1
+        answer = binary_search.binary_search(arr, val, min_pos, max_pos)
+        self.assertEqual(True, answer)
+    
+    def test_binary_search_even_false(self):
+        arr = [5,9,10,73]
+        val = 6
+        min_pos = 0
+        max_pos = len(arr) - 1
+        answer = binary_search.binary_search(arr, val, min_pos, max_pos)
+        self.assertEqual(False, answer)
+    
+    def test_binary_search_odd_true(self):
+        arr = [5,9,10,55,73]
+        val = 5
+        min_pos = 0
+        max_pos = len(arr) - 1
+        answer = binary_search.binary_search(arr, val, min_pos, max_pos)
+        self.assertEqual(True, answer)
+    
+    def test_binary_search_odd_false(self):
+        arr = [5,9,10,55,73]
+        val = 89
+        min_pos = 0
+        max_pos = len(arr) - 1
+        answer = binary_search.binary_search(arr, val, min_pos, max_pos)
+        self.assertEqual(False, answer)
